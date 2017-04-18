92f2286002d88dde14247a2b463ec713c2abdb5a
benweizhu <benweizhu@gmail.com>
2017-04-18 12:05:10 +0800

    change example

--- example/index.jsx
+++ example/index.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import PropTypes from 'prop-types';
 import ReactHeatmap from 'react-heatmap';
 
 class ReactHeatmapExample extends React.Component {
-  render(){
+  render() {
+    var points = [];
+    var max = 0;
+    var width = 840;
+    var height = 400;
+    var len = 300;
+
+    while (len--) {
+      var val = Math.floor(Math.random() * 100);
+      // now also with custom radius
+      var radius = Math.floor(Math.random() * 70);
+
+      max = Math.max(max, val);
+      var point = {
+        x: Math.floor(Math.random() * width),
+        y: Math.floor(Math.random() * height),
+        value: val,
+        // radius configuration on point basis
+        radius: radius
+      };
+      points.push(point);
+    }
+
     return (
-      <div><ReactHeatmap /></div>
+      <div style={{ width: '840px', height: '400px' }}>
+        < ReactHeatmap max= { max } data= { points } unit= "notPercent" />
+      </div >
     );
   }
 }
 
-ReactDom.render(<ReactHeatmapExample/>, document.querySelector('#app'));
+ReactDom.render(<ReactHeatmapExample />, document.querySelector('#app'));
