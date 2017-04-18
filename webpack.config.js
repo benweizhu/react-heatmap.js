abd9c6767e9f31c4ccb31776860a9f97857e0cc6
benweizhu <benweizhu@gmail.com>
2017-04-18 10:19:47 +0800

    add babel webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
   output: {
     filename: 'ReactHeatmap.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx|.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
   }
+
 };
